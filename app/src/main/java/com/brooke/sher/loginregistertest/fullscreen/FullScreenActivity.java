32526897dfebb7fbb5418d6761d6c6deadf53c02
Sher <wang568212103@gmail.com>
2018-04-06 00:03:13 +0800

    fix something

---
+++ app/src/main/java/com/brooke/sher/loginregistertest/fullscreen/FullScreenActivity.java
@@ -0,0 +1,15 @@
+package com.brooke.sher.loginregistertest.fullscreen;
+
+import android.os.Bundle;
+
+import com.brooke.sher.app2.ui.BaseAppActivity;
+import com.brooke.sher.loginregistertest.R;
+
+public class FullScreenActivity extends BaseAppActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_screen);
+    }
+}
